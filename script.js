e2670e92784e9cfe02dc41c0d902e2cec7d67687
Tristan Beley <tbeley@uwo.ca>
2024-11-08 14:46:08 -0500

    Finished Portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const m = document.querySelector(".menu-links");
+    const i = document.querySelector(".fall-icon");
+    m.classList.toggle("open");
+    i.classList.toggle("open");
+}
