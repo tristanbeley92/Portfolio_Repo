f722e595a144191ea0fb9126363f7f677e0a5088
Tristan Beley <tbeley@uwo.ca>
2025-09-05 19:02:12 -0400

    Content/Style Update

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu(){
     m.classList.toggle("open");
     i.classList.toggle("open");
 }
+
+function scrollToNext() {
+    const skillsSection = document.getElementById('skills');
+    const offset = -110; // adjusted for proper scrolling to the skills section 
+    const y = skillsSection.getBoundingClientRect().top + window.scrollY - offset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
+  }
+
+
+
